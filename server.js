c85c07d0c4501a55c0f2c43f9a710781ad59b09a
PacDraft <Kirsten@PacificDraftWorks.com>
2022-02-27 19:42:13 -0800

    started code in server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require ("express");
+const api = require("./routing/api");
+const html = require("./routing/html");
+
+const notetaker = express();
+const PORT = process.env.PORT || 3001;
+
+notetaker.use(express.json());
+notetaker.use(express.urlencoded({ extended: true }));
+notetaker.use(express.static("public"));
