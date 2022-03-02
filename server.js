16d9e79eda5a8396087cc06ef045c2a6e6c9c44f
PacDraft <Kirsten@PacificDraftWorks.com>
2022-03-02 13:19:35 -0800

    worked on server.js file

--- server.js
+++ server.js
@@ -8,3 +8,7 @@ const PORT = process.env.PORT || 3001;
 notetaker.use(express.json());
 notetaker.use(express.urlencoded({ extended: true }));
 notetaker.use(express.static("public"));
+app.use("/api", api);
+app.use("/", html);
+
+app.listen(PORT, () => console.log(`App is listening on PORT: ${PORT}`));
