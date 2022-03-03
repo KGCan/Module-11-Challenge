2ce69c10174c1e9f9d0a3f1b8688363bdc939822
PacDraft <Kirsten@PacificDraftWorks.com>
2022-03-02 21:01:37 -0800

    corrected typo in server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 taskmanager.use(express.json());
 taskmanager.use(express.urlencoded({ extended: true }));
 taskmanager.use(express.static("public"));
-app.use("/api", api);
-app.use("/", html);
+taskmanager.use("/api", api);
+taskmanager.use("/", html);
 
-app.listen(PORT, () => console.log(`App is listening on PORT: ${PORT}`));
+taskmanager.listen(PORT, () => console.log(`App is listening on PORT: ${PORT}`));
